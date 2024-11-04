0607e93185779f55df3cde9248ba20ad05d2b0eb
Guiii-lz <guima.luiz@hotmail.com>
2024-11-04 19:09:29 -0300

    first commit

---
+++ src/test/java/io/github/guiiilz/Register/RegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.guiiilz.Register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
